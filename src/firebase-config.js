9e0fc057ee2901a872b47e45656b6afd2a8b4386
ElodieSayavong <elodie.sayavong.dev@gmail.com>
2022-03-03 13:49:44 +0100

    firebase connection

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+    apiKey: "AIzaSyAryL3XrbyVqD4iq_8X5hqUy3iFRs567i4",
+    authDomain: "host-and-go.firebaseapp.com",
+    projectId: "host-and-go",
+    storageBucket: "host-and-go.appspot.com",
+    messagingSenderId: "345259792323",
+    appId: "1:345259792323:web:ac1066ab856d4f4552cf76",
+    measurementId: "G-PVXV3DYCHK"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export default getFirestore();
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+import { onSnapshot, collection } from "firebase/firestore";
+import db from "../../firebase-config";
+
 const Login = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(
+        () =>
+            onSnapshot(collection(db, "restaurant"), (snapshot) =>
+                setData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            ),
+        []
+    );
+
     return (
         <div>
             Login
